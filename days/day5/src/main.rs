8b2f5a21d6275666e9ea8ac690582a907175c2d9
Viktor Kasimir <viktor@voysys.se>
2024-12-05 10:54:50 +0100

    opt

--- days/day5/src/main.rs
+++ days/day5/src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering, time::Instant};
+use std::{cmp::Ordering, collections::HashSet, time::Instant};
 
 const INPUT: &str = include_str!("input.txt");
 
@@ -6,7 +6,7 @@ fn main() {
     let start = Instant::now();
 
     let mut updates = Vec::new();
-    let mut rules = Vec::new();
+    let mut rules = HashSet::new();
 
     let mut first_section = true;
     for line in INPUT.lines() {
@@ -16,21 +16,17 @@ fn main() {
         }
 
         if first_section {
-            let mut split = line.split_terminator("|");
+            let mut split = line.split("|");
 
             let a = split.next().unwrap().parse::<i32>().unwrap();
             let b = split.next().unwrap().parse::<i32>().unwrap();
 
-            rules.push((a, b));
+            rules.insert((a, b));
         } else {
-            let split = line.split_terminator(",");
-
-            let mut update = Vec::new();
-
-            for v in split {
-                update.push(v.parse::<i32>().unwrap());
-            }
-
+            let update = line
+                .split(',')
+                .map(|v| v.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
             updates.push(update);
         }
     }
@@ -38,26 +34,17 @@ fn main() {
     let mut p1 = 0;
     let mut p2 = 0;
     for update in updates.iter_mut() {
-        let mut is_correct = true;
-
-        for window in update.windows(2) {
-            let prev = window[0];
-            let next = window[1];
-
-            if !rules.iter().any(|(r1, r2)| prev == *r1 && next == *r2) {
-                is_correct = false;
-                break;
-            }
-        }
+        let is_correct = update
+            .windows(2)
+            .all(|window| rules.contains(&(window[0], window[1])));
 
         if is_correct {
             let middle = update[update.len() / 2];
-            assert!(update.len() % 2 == 1); // All Odd
 
             p1 += middle;
         } else {
             update.sort_unstable_by(|v1, v2| {
-                if rules.iter().any(|(r1, r2)| *v1 == *r1 && *v2 == *r2) {
+                if rules.contains(&(*v1, *v2)) {
                     Ordering::Less
                 } else {
                     Ordering::Greater
@@ -68,7 +55,8 @@ fn main() {
             p2 += middle;
         }
     }
+
     println!("Time: {}", start.elapsed().as_micros());
-    println!("Part 1: {p1}");
-    println!("Part 2: {p2}");
+    println!("Part 1: {p1}, Correct: 4924");
+    println!("Part 2: {p2}, Correct: 6085");
 }
