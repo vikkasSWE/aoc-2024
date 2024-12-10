5f509e14708f8614cdc7a69bbc711e46f5fd73b2
Viktor Kasimir <viktor@voysys.se>
2024-12-10 08:20:07 +0100

    day10 b

--- days/day10/src/lib.rs
+++ days/day10/src/lib.rs
@@ -19,7 +19,7 @@ pub fn a() -> u32 {
     let mut score = 0;
     for start in starting_positions {
         let mut used = HashSet::new();
-        let res = find_trail(start, &map, &mut used, 0);
+        let res = find_trail(start, &map, &mut used, 0, false);
         score += res;
     }
 
@@ -31,10 +31,11 @@ fn find_trail(
     map: &HashMap<(usize, usize), u32>,
     used: &mut HashSet<(usize, usize)>,
     depth: u32,
+    part_b: bool,
 ) -> u32 {
     if depth == 9 {
         if used.contains(&pos) {
-            return 0;
+            return part_b as u32;
         } else {
             used.insert(pos);
             return 1;
@@ -50,31 +51,51 @@ fn find_trail(
 
     if let Some(down) = map.get(&down_one) {
         if *down == depth + 1 {
-            score += find_trail(down_one, map, used, depth + 1);
+            score += find_trail(down_one, map, used, depth + 1, part_b);
         }
     }
 
     if let Some(right) = map.get(&right_one) {
         if *right == depth + 1 {
-            score += find_trail(right_one, map, used, depth + 1);
+            score += find_trail(right_one, map, used, depth + 1, part_b);
         }
     }
 
     if let Some(left) = map.get(&left_one) {
         if *left == depth + 1 {
-            score += find_trail(left_one, map, used, depth + 1);
+            score += find_trail(left_one, map, used, depth + 1, part_b);
         }
     }
 
     if let Some(up) = map.get(&up_one) {
         if *up == depth + 1 {
-            score += find_trail(up_one, map, used, depth + 1);
+            score += find_trail(up_one, map, used, depth + 1, part_b);
         }
     }
 
     score
 }
 
-pub fn b() -> usize {
-    0
+pub fn b() -> u32 {
+    let mut map = HashMap::new();
+    let mut starting_positions = Vec::new();
+
+    for (row, line) in INPUT.lines().enumerate() {
+        for (col, c) in line.chars().enumerate() {
+            let value = c.to_digit(10).unwrap_or(20);
+            map.insert((row, col), value);
+            if value == 0 {
+                starting_positions.push((row, col));
+            }
+        }
+    }
+
+    let mut score = 0;
+    for start in starting_positions {
+        let mut used = HashSet::new();
+        let res = find_trail(start, &map, &mut used, 0, true);
+        score += res;
+    }
+
+    score
 }
